a73ce2fafe186f9071a0000d6516d5341fdffadb
seckinemrah <seckinemrah@gmail.com>
2020-07-25 16:11:23 -0500

    edited setnv

--- deployments/terraform/setenv.sh
+++ deployments/terraform/setenv.sh
@@ -66,7 +66,7 @@ cat << EOF > "$DIR/backend.tf"
 terraform {
     backend "s3" {
         bucket = "${S3BUCKET}"
-        key = "${S3BUCKETPROJ}/${S3BUCKETREGION}/${S3BUCKETTYPE}/${ENVIRONMENT}/${S3TFSTATEFILE}"
+        key = "${ENVIRONMENT}/${S3TFSTATEFILE}"
         region = "${S3BUCKETREGION}"
     }
 }
