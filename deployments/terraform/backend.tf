7c53abe1178d0508b302506df5d665c9995987fe
seckinemrah <seckinemrah@gmail.com>
2020-07-25 14:56:55 -0500

    edited backend

--- deployments/terraform/backend.tf
+++ deployments/terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "s3" {
-        bucket = "seckinemrah-jenkins-bucket"
+        bucket = "seckinemrah-234"
         key = "jenkins/us-east-1/class/dev/infrastructure.tfstate"
-        region = "us-east-1"
+        region = "us-east-2"
     }
 }
 
